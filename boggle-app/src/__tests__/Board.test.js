9d7c75df843e91b73212bee9ab82c0cdb008fd30
LUM-K-C-CHOH <156261770+LUM-K-C-CHOH@users.noreply.github.com>
2025-12-10 10:18:06 -0500

    Remove old tests and add clean smoke tests

--- boggle-app/src/__tests__/Board.test.js
+++
@@ -1,6 +0,0 @@
-import { render } from "@testing-library/react";
-import Board from "../Board";
-
-test("Board renders without crashing", () => {
-  render(<Board board={[]} />);
-});
--- boggle-app/src/__tests__/GameState.test.js
+++
@@ -1,5 +0,0 @@
-import GameState from "../GameState";
-
-test("GameState module loads", () => {
-  expect(GameState).toBeDefined();
-});
