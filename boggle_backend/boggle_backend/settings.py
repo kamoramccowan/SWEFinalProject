fb0c1b0619d1935a84cc766d71d43189b6205785
Iosman12 <Ibrahim.osman@bison.howard.edu>
2025-12-10 17:04:41 -0500

    Fix CORS headers

--- boggle_backend/boggle_backend/settings.py
+++ boggle_backend/boggle_backend/settings.py
@@ -35,7 +35,17 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CORS_ALLOW_ALL_ORIGINS = os.environ.get('CORS_ALLOW_ALL', 'False').lower() == 'true'
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 CORS_ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
 
 
